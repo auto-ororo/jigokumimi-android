d26393957dcd609225355a62e8a5e372fe5e1df9
ororo <farewellbbbb@gmail.com>
2020-04-27 15:30:15 +0900

    バグ修正 ･アイコン変更 ･IllegalSateException修正

--- app/src/main/java/com/ororo/auto/jigokumimi/viewmodels/SearchViewModel.kt
+++ app/src/main/java/com/ororo/auto/jigokumimi/viewmodels/SearchViewModel.kt
@@ -48,70 +48,76 @@ class SearchViewModel(
      * 周辺曲情報を更新する
      */
     fun searchMusic() {
-        viewModelScope.launch(Dispatchers.IO) {
-            Timber.d("Search Music called")
-            try {
-                // 位置情報を取得する
-                val flow = locationRepository.getCurrentLocation()
-                flow.collect { location: Location ->
-                    Timber.d("緯度:${location.latitude}, 経度:${location.longitude}")
 
-                    // JigokumimiのユーザーIDを取得する
-                    val jigokumimiUserId = authRepository.getSavedJigokumimiUserId()
+        // 位置情報を取得する
+        viewModelScope.launch {
+            val flow = locationRepository.getCurrentLocation()
+            flow.collect { location: Location ->
+                viewModelScope.launch(Dispatchers.IO) {
+                    Timber.d("Search Music called")
+                    try {
+                        Timber.d("緯度:${location.latitude}, 経度:${location.longitude}")
 
-                    if (searchType.value == Constants.SearchType.TRACK) {
-                        // 検索種別が曲の場合
+                        // JigokumimiのユーザーIDを取得する
+                        val jigokumimiUserId = authRepository.getSavedJigokumimiUserId()
 
-                        // ユーザーのお気に入り曲一覧を取得し､リクエストを作成
-                        val postTracks = musicRepository.getMyFavoriteTracks()
-                            .asPostMyFavoriteTracksRequest(jigokumimiUserId, location)
+                        if (searchType.value == Constants.SearchType.TRACK) {
+                            // 検索種別が曲の場合
 
-                        // Jigokumimiにお気に入り曲リストを登録
-                        musicRepository.postMyFavoriteTracks(postTracks)
+                            // ユーザーのお気に入り曲一覧を取得し､リクエストを作成
+                            val postTracks = musicRepository.getMyFavoriteTracks()
+                                .asPostMyFavoriteTracksRequest(jigokumimiUserId, location)
 
-                        // 周りのJigokumimiユーザーのお気に入り曲を取得
-                        musicRepository.refreshTracks(jigokumimiUserId, location, _distance.value!!)
+                            // Jigokumimiにお気に入り曲リストを登録
+                            musicRepository.postMyFavoriteTracks(postTracks)
 
-                        // 取得件数が0件の場合はエラー表示
-                        if (musicRepository.tracks.value?.size == 0) {
-                            showMessageDialog(getApplication<Application>().getString(R.string.no_tracks_error_message))
+                            // 周りのJigokumimiユーザーのお気に入り曲を取得
+                            musicRepository.refreshTracks(
+                                jigokumimiUserId,
+                                location,
+                                _distance.value!!
+                            )
+
+                            // 取得件数が0件の場合はエラー表示
+                            if (musicRepository.tracks.value?.size == 0) {
+                                showMessageDialog(getApplication<Application>().getString(R.string.no_tracks_error_message))
+                            } else {
+                                // 検索完了フラグをON
+                                _isSearchFinished.postValue(true)
+                            }
+
                         } else {
-                            // 検索完了フラグをON
-                            _isSearchFinished.postValue(true)
+                            // 検索種別がアーティストの場合
+
+                            // ユーザーのお気に入りアーティスト一覧を取得し､リクエストを作成
+                            val postArtists = musicRepository.getMyFavoriteArtists()
+                                .asPostMyFavoriteArtistsRequest(jigokumimiUserId, location)
+
+                            // Jigokumimiにお気に入り曲リストを登録
+                            musicRepository.postMyFavoriteArtists(postArtists)
+
+                            // 周りのJigokumimiユーザーのお気に入り曲を取得
+                            musicRepository.refreshArtists(
+                                jigokumimiUserId,
+                                location,
+                                _distance.value!!
+                            )
+
+                            // 取得件数が0件の場合はエラー表示
+                            if (musicRepository.artists.value?.size == 0) {
+                                showMessageDialog(getApplication<Application>().getString(R.string.no_artists_error_message))
+                            } else {
+                                // 検索完了フラグをON
+                                _isSearchFinished.postValue(true)
+                            }
                         }
+                        Timber.d("Search Music Succeeded")
 
-                    } else {
-                        // 検索種別がアーティストの場合
-
-                        // ユーザーのお気に入りアーティスト一覧を取得し､リクエストを作成
-                        val postArtists = musicRepository.getMyFavoriteArtists()
-                            .asPostMyFavoriteArtistsRequest(jigokumimiUserId, location)
-
-                        // Jigokumimiにお気に入り曲リストを登録
-                        musicRepository.postMyFavoriteArtists(postArtists)
-
-                        // 周りのJigokumimiユーザーのお気に入り曲を取得
-                        musicRepository.refreshArtists(
-                            jigokumimiUserId,
-                            location,
-                            _distance.value!!
-                        )
-
-                        // 取得件数が0件の場合はエラー表示
-                        if (musicRepository.artists.value?.size == 0) {
-                            showMessageDialog(getApplication<Application>().getString(R.string.no_artists_error_message))
-                        } else {
-                            // 検索完了フラグをON
-                            _isSearchFinished.postValue(true)
-                        }
+                    } catch (e: Exception) {
+                        handleConnectException(e)
                     }
 
-
-                    Timber.d("Search Music Succeeded")
                 }
-
-            } catch (e: Exception) {
-                handleConnectException(e)
             }
         }
     }
